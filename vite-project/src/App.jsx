b0f3601690509ae83b8b998eeca808f4276349d7
Pablo D <pabloscharfspitz@hotmail.com>
2023-10-26 15:22:59 -0300

    3comt

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,3 +34,12 @@ function App() {
 }
 
 export default App
+/* ------------------------------------------ */
+function App() {
+  return (
+    <div>
+      <Navbar /> {}
+    </div>
+  );
+}
+
